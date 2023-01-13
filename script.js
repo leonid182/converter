7d371ad6a7915da978aee8199d42903d599c49fa
unknown <blink_182@inbox.ru>
2023-01-14 01:14:51 +0300

    6

--- script.js
+++ script.js
@@ -5,11 +5,17 @@ let div = document.querySelector('.div');
 let range = document.getElementById('input__second');
 let div__second = document.querySelector('.div__second');
 
-
+let input__third = document.getElementById('input__third');
+let select__third = document.getElementById('select__third');
+let div__third = document.querySelector('.div__third');
 
 let task = {
     eur: 0.014,
     usd: 0.013
+};
+let far = {
+    mile: 0.0006,
+    foot: 3.28
 };
 
 
@@ -23,6 +29,14 @@ let exchenge = (rub, current)=>{
     return(rub*task[current]);
 }
 
+let convert = (metr, current) => {
+    if(metr<0){
+        return null
+    }
+
+    return(metr*far[current]);
+}
+
 input.addEventListener('input', ()=>{
    let result =  exchenge(input.value, select.value);
     div.innerHTML = Math.floor(result * 100) / 100
@@ -31,6 +45,16 @@ input.addEventListener('input', ()=>{
 select.addEventListener('change', ()=>{
     let result =  exchenge(input.value, select.value);
     div.innerHTML = Math.floor(result * 100) / 100
+
+});
+input__third.addEventListener('input', ()=>{
+   let result =  convert(input__third.value, select__third.value);
+    div__third.innerHTML = Math.floor(result * 100) / 100
+
+});
+select__third.addEventListener('change', ()=>{
+    let result =  convert(input__third.value, select__third.value);
+    div__third.innerHTML = Math.floor(result * 100) / 100
 
 });
 

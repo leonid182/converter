34b80cb74e034b96f270b17b4d4638aa31b84b37
unknown <blink_182@inbox.ru>
2023-01-13 21:52:49 +0300

    5

--- script.js
+++ script.js
@@ -1,7 +1,11 @@
 let input = document.getElementById('input');
 let select = document.getElementById('select');
-
 let div = document.querySelector('.div');
+
+let range = document.getElementById('input__second');
+let div__second = document.querySelector('.div__second');
+
+
 
 let task = {
     eur: 0.014,
@@ -23,11 +27,19 @@ input.addEventListener('input', ()=>{
    let result =  exchenge(input.value, select.value);
     div.innerHTML = Math.floor(result * 100) / 100
 
-})
+});
 select.addEventListener('change', ()=>{
     let result =  exchenge(input.value, select.value);
     div.innerHTML = Math.floor(result * 100) / 100
 
+});
+
+range.addEventListener('input', ()=>{
+    div__second.innerHTML = range.value;
+    if (div__second.innerHTML ==60) {
+        alert('60')
+    }
 })
+
 
 
